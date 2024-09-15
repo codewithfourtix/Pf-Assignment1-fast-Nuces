4306567ce8d3e9a9e934446dc133f3550fb3c88d
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-09-15 16:26:34 +0500

    Renaming

---
+++ PF_Q8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Number_1, Number_2;
+
+    cout << "Enter two Numbers : ";
+    cin >> Number_1 >> Number_2;
+
+    if (Number_1 % Number_2 == 0) {
+        cout << Number_1 << " is a multiple of " << Number_2 << endl;
+    }else if (Number_2 % Number_1 == 0) {
+        cout << Number_2 << " is a multiple of " << Number_1 << endl;
+    }else {
+        cout << "NON" << endl;
+    }
+
+    return 0;
+}
+
